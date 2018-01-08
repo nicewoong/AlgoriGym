f163b8fec91219864a291ff753b69c037a8f514e
nicewoong <thoughtscode@gmail.com>
2018-01-08 23:00:26 +0900

    Created algoriGym

---
+++ cutting_brackets.cpp
@@ -0,0 +1,26 @@
+//
+//  cutting_brackets.cpp
+//  algorithm_discipline
+//
+//  Created by nicewoong on 2018. 1. 8..
+//  Copyright © 2018년 nicewoong. All rights reserved.
+//
+//  See : https://www.acmicpc.net/problem/10799
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int input;
+    string word;
+    cout<< "hello\n";
+    cin >> input;
+    cout << input;
+    cin >> word;
+    cout << word;
+    
+    
+    
+    
+}
---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  algorithm_discipline
+//
+//  Created by nicewoong on 2018. 1. 8..
+//  Copyright © 2018년 nicewoong. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
