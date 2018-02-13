e529eaf3399ab01e01a0b2c90121b2e10f6e1dc7
nicewoong <thoughtscode@gmail.com>
2018-02-13 21:02:34 +0900

    Update 2156.cpp

--- 2156.cpp
+++ 2156.cpp
@@ -16,29 +16,32 @@
 using namespace std;
 
 
-long long d[MAX_N + 1][2] = {0};
+long long d[MAX_N + 1][2] = {0}; // 0: n 번째 포토주를 안 먹었을 때. 1 : n 번째 포도주를 먹었을 때 
 int wine[MAX_N + 1] = {0};
 
 
 long long func(int N) {
-    d[1][1] = wine[1];
+    d[1][1] = wine[1];  
     d[1][0] = 0;
     d[2][1] = wine[1] + wine[2];
     d[2][0] = wine[1];
     
     for(int n = 3 ; n <= N; n++) {
+        
+        // n 번째 포토주를 안 먹었을 때.
         if( d[n-1][0] > d[n-1][1])
-            d[n][0] = d[n-1][0];
+            d[n][0] = d[n-1][0];  // 안 먹고, 안 먹을 경우
         else
-            d[n][0] = d[n-1][1];
+            d[n][0] = d[n-1][1];  // 먹고, 안 먹을 
         
-        long long max = d[n-2][0] + wine[n];
-        if(d[n-2][0] + wine[n-1] + wine[n] > max)
-            max = d[n-2][0] + wine[n-1] + wine[n];
+        // n 번째 포도주를 먹었을 때 - 세 경우 중 max 값 
+        long long max = d[n-2][0] + wine[n];  // 안 먹고, 안 먹고, 먹을 경우
+        if(d[n-2][0] + wine[n-1] + wine[n] > max)  
+            max = d[n-2][0] + wine[n-1] + wine[n];  // 안 먹고, 먹고, 먹을 경우
         if(d[n-2][1] + wine[n] > max)
-            max = d[n-2][1] + wine[n];
+            max = d[n-2][1] + wine[n];  // 먹고, 안 먹고, 먹을 경우
         
-        d[n][1] = max;
+        d[n][1] = max;  // 세 경우 중 max 값 
     } // end of for
     
     // return max sum
